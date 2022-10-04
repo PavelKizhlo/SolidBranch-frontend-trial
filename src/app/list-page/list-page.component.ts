ab502003203a295a9b4f04c76434b8b1fa8c15d0
Pavel Kizhlo <pavelkizh@gmail.com>
2022-10-04 11:59:42 +0300

    feat: implement transactions filtration by type

--- src/app/list-page/list-page.component.ts
+++ src/app/list-page/list-page.component.ts
@@ -1,7 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { TransactionItem, TransactionType } from '../shared/interfaces';
+import { transactions } from '../shared/transaction-list';
 
 @Component({
   selector: 'app-list-page',
   templateUrl: './list-page.component.html',
 })
-export class ListPageComponent {}
+export class ListPageComponent implements OnInit {
+  transactions: TransactionItem[] = transactions.data;
+
+  filteredTransactions: TransactionItem[] = [];
+
+  transactionType: TransactionType = 'income';
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      switch (params['tab']) {
+        case '1':
+          this.transactionType = 'outcome';
+          break;
+        case '2':
+          this.transactionType = 'loan';
+          break;
+        case '3':
+          this.transactionType = 'investment';
+          break;
+        case '0':
+        default:
+          this.transactionType = 'income';
+          break;
+      }
+
+      this.filteredTransactions = this.transactions.filter(
+        (transaction) => transaction.type === this.transactionType,
+      );
+    });
+  }
+}
